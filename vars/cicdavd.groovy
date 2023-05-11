b6f5447301aee527b4450c0d439d049380220250
swaroop-tech <127303191+swaroop-tech@users.noreply.github.com>
2023-05-11 15:13:19 +0530

    cicd p.

---
+++ vars/cicdavd.groovy
@@ -0,0 +1,16 @@
+def contDownload(repo)
+{
+  git "https://github.com/prasadcloud/${repo}"
+}
+def conuBuild()
+{
+  sh 'mvn package'
+}
+def contDep(jobname,ipaddress,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
+}
+def conTest(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
